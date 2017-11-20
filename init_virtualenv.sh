6465a1c9b2a944adb63a904cd34c34cabd8e292f
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-11-20 15:07:30 +0100

    add iris example

--- init_virtualenv.sh
+++ init_virtualenv.sh
@@ -10,3 +10,4 @@ pip install keras==2.1.1
 pip install h5py==2.7.1
 pip install matplotlib==2.1.0
 pip install pypng==0.0.18
+pip install scikit-learn==0.19.1
---
+++ keras/iris/train.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+# Select Theano as backend for Keras
+from os import environ
+environ["KERAS_BACKEND"] = "tensorflow"
+
+import numpy as np
+np.random.seed(1234)
+from sklearn.datasets import load_iris
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import SGD
+
+# Load iris dataset
+dataset = load_iris()
+inputs = dataset["data"]
+target_names = dataset["target_names"]
+targets = dataset["target"]
+targets_onehot = np_utils.to_categorical(targets, len(target_names))
+
+print("Loaded iris dataset with {} entries.".format(inputs.shape[0]))
+print("Example entry: {}".format(inputs[0]))
+
+# Define model
+model = Sequential()
+
+model.add(Dense(
+    8, # Number of nodes
+    kernel_initializer="glorot_normal", # Initialization
+    activation="relu", # Activation
+    input_dim=inputs.shape[1])) # Shape of inputs (only needed for the first layer)
+
+model.add(Dense(
+    len(target_names),
+    kernel_initializer="glorot_uniform",
+    activation="softmax"))
+
+model.summary()
+
+# Set loss, optimizer and evaluation metrics
+model.compile(
+        loss="categorical_crossentropy",
+        optimizer=SGD(lr=0.10),
+        metrics=["accuracy",])
+
+# Train
+history = model.fit(inputs, targets_onehot, batch_size=20, epochs=10)
+
+# Validate
+predictions = model.predict(inputs)
+predictions_argmax = np.argmax(predictions, axis=1)
+accuracy = np.sum(predictions_argmax==targets)/float(inputs.shape[0])
+print("Accuracy on full dataset: {}".format(accuracy))
+
+# Save model
+model.save("weights.h5")
+
+# Plot loss and accuracy over epochs
+loss = history.history['loss']
+acc = history.history['acc']
+epochs = range(1, 1+len(loss))
+
+fig, ax1 = plt.subplots()
+
+ax1.set_xlabel("Epochs")
+ax1.set_ylabel("Loss")
+ax1.plot(epochs, loss, 'rs', label="Loss")
+
+ax2 = ax1.twinx()
+ax2.set_ylabel("Accuracy")
+ax2.plot(epochs, acc, 'bo', label="Accuracy")
+
+plt.savefig("iris_epochs.png")
