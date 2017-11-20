da2315599e246135ba8930a3a29612abaa1358a1
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-11-20 12:23:05 +0100

    add matplotlib and fix h5py version

--- init_virtualenv.sh
+++ init_virtualenv.sh
@@ -7,4 +7,5 @@ source $VIRTUALENV_PATH/bin/activate
 pip install --upgrade pip
 pip install tensorflow==1.4.0
 pip install keras==2.1.1
-pip install h5py
+pip install h5py==2.7.1
+pip install matplotlib==2.1.0
